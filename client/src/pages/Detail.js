748d3f3ba07bf9aca518c98aaebcb797abd8ae74
Robert Greenawalt <robert.greenawalt29@gmail.com>
2021-01-23 22:32:38 -0500

    Added initial files

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+
+function Detail() {
+
+  let {id} = useParams();
+
+  const [book, setBook] = useState({})
+  
+  // Add code to get the book with an _id equal to the id in the route param
+  // e.g. http://localhost:3000/books/:id
+  // The book id for this route can be accessed using the useParams hook
+  // from react-router-dom.
+  useEffect(() => {
+    API.getBook(/* book id should be passed here */id )
+      .then(res => setBook(res.data))
+      .catch(err => console.log(err));
+  }, [])
+
+  return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>
+                {book.title} by {book.author}
+              </h1>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-10 md-offset-1">
+            <article>
+              <h1>Synopsis</h1>
+              <p>
+                {book.synopsis}
+              </p>
+            </article>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-2">
+            <a href="/">← Back to Authors</a></Col>
+        </Row>
+      </Container>
+    );
+  }
+
+
+export default Detail;
